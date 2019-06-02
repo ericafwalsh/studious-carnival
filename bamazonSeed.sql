bfc31c02ae3a5ba9f6449f27773e1b0e744a4178
Erica Walsh <erica.f.walsh@gmail.com>
2019-06-02 09:23:53 -0400

    updates

---
+++ bamazonSeed.sql
@@ -0,0 +1,7 @@
+
+Insert into products (product_name, department_name, price, stock_quantity)
+values ("glasses", "accessories", 15, 20),("water bottle", "sporting goods", 7, 30),("head phones", "electronics", 50, 10),("cell phone charger", "electronics", 10, 30),("running watch", "sporting goods", 120, 13),
+("cat bed", "pets", 25, 27),("slippers", "accessories", 22, 15),("speakers", "electronics", 67, 41),("notepad", "office suppliers", 7, 46),("chocolate bar", "grocery", 6, 22);
+
+insert into departments (department_name, over_head_costs)
+values ("accessories",2000),("sporting goods",100),("electronics",4000),("pets",10000),("office supplies",3000),("grocery",30000);
